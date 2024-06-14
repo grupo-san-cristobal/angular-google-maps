9043f21096a1159c705b69761006b83875cf350e
Sol Lopez <sol.lopez@gruposancristobal.com>
2024-06-14 16:33:45 -0300

    versioning and test fixes

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
+
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
 module.exports = {
   preset: 'jest-preset-angular',
   //https://github.com/thymikee/jest-preset-angular/issues/167#issuecomment-459686655
   modulePathIgnorePatterns: ["<rootDir>/dist/"],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    "@grupo-san-cristobal/agm-core": "<rootDir>/node_modules/@grupo-san-cristobal/agm-core/src/public-api"
+  }
 };
